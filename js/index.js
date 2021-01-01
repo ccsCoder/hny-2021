4ffcd5527ad9d13c705ccc181a8cd7261e41c063
Nishant <ccsCoder@gmail.com>
2021-01-01 19:14:56 +0530

    A simple greeting

---
+++ js/index.js
@@ -0,0 +1,35 @@
+const config = {
+  selector: '.background',
+  maxParticles: 300,
+  speed: 0.4,
+  sizeVariations: 7,
+  color: ['#ffff66', '#ffffb3', '#ffffcc'],
+  responsive: [{
+    breakpoint: 768,
+    options: {
+      maxParticles: 200,
+      sizeVariations: 5,
+    }
+  }]
+};
+
+let audio = null;
+
+window.onload= function() {
+  audio = new Audio('../res/forest.mp3');
+};
+
+document.addEventListener('DOMContentLoaded', e => {
+  document.querySelector('#greet-button').addEventListener('click', e => {
+    // start off the audio in the background.
+    audio.addEventListener('ended', function() {
+      this.currentTime = 0;
+      this.play();
+    }, false);
+    audio.play();
+    // start hiding stuff.
+    document.querySelector('.intro-background').style.opacity = 0;
+    document.querySelector('.intro-background').addEventListener('transitionend', function() {this.style.display = 'none';});
+    Particles.init(config);
+  });
+});
